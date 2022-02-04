284e60a3e78b316ea2a7a939459d43ef9eccde96
Asyraf Rahim <asyrafboyz@gmail.com>
2022-02-04 12:18:21 +0800

    API routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => '\App\Http\Controllers'], function () {
+
+    ## Auth
+    Route::post('login', 'API\AuthController@login');
+    Route::post('register', 'API\AuthController@register');
+    Route::get('/logout', 'API\AuthController@logout');
+
+    ## User
+    Route::get('/user', 'API\UserController@index')->name('api:user:index');
+
+    ## Post
+    Route::get('/posts', 'API\PostController@index')->name('api:post:index');
+    Route::post('/posts/create', 'API\PostController@store')->name('api:post:store');
+    Route::get('/posts/{post}', 'API\PostController@show')->name('api:post:show');
+    
+    ## Comment
+    Route::get('/posts/{post}/comments', 'API\CommentController@index')->name('api:comment:index');
+    Route::post('/posts/{post}/comments/create', 'API\CommentController@store')->name('api:comment:store');
+    Route::post('/posts/{post}/comments/reply', 'API\CommentController@replyComment')->name('api:comment:reply');
 });
+
+
